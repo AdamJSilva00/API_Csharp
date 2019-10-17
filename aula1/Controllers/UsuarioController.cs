5948ca7ab4dcb2bb400d9dd8930ef0dba7dd881b
Gabriel Moraes <gmmartins06@gmail.com>
2019-10-17 11:46:41 -0300

    Lista com dados em formato de API

---
+++ aula1/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using aula1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aula1.Controllers
+{
+    [ApiController] // Mostra que é uma api
+    [Route("v1/[controller]")] // Define a rota -> nome da rota
+    [Produces("application/json")] // Define o tipo de dado transmitido -> nesse caso é json
+    public class UsuarioController : ControllerBase
+    {
+        // ActionResult -> mostra o resultado de uma ação
+        public IActionResult Usuarios()
+        {
+            List<UsuarioModel> listaDeUsuarios = new List<UsuarioModel>();
+
+            // Criando um usuario
+            UsuarioModel usuario    = new UsuarioModel();
+            usuario.id_usuario      = 1;
+            usuario.nome_usuario    = "Gabriel Moraes";
+            usuario.email           = "gmmartins06@gmail.com";
+            usuario.senha           = "123";
+
+            UsuarioModel usuario2   = new UsuarioModel();
+            usuario2.id_usuario     = 2;
+            usuario2.nome_usuario   = "Gabriella Evangelista";
+            usuario2.email          = "gabi.evangelista07@gmail.com";
+            usuario2.senha          = "456";
+
+            // Adicionando na lista de usuarios 
+
+            listaDeUsuarios.Add(usuario);
+            listaDeUsuarios.Add(usuario2);
+
+            // Retornar a lista de usuarios
+            return Ok(listaDeUsuarios);
+        }
+    }
+}
---
+++ aula1/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+namespace aula1.Models
+{
+    public class UsuarioModel
+    {
+        public int id_usuario { get; set; }
+        public string nome_usuario { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        
+    }
+}
