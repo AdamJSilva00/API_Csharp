258c1264dd384f195ede9ad05d54aed0f2c52b48
Gabriel Moraes <gmmartins06@gmail.com>
2019-10-17 14:41:35 -0300

    Passando o id por meio da URL e mostrando todas as informações

--- aula1/Controllers/UsuarioController.cs
+++ aula1/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using aula1.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,23 +10,26 @@ namespace aula1.Controllers
     [Produces("application/json")] // Define o tipo de dado transmitido -> nesse caso é json
     public class UsuarioController : ControllerBase
     {
+        // Lista de usuarios global
+        List<UsuarioModel> listaDeUsuarios = new List<UsuarioModel>();
+
+        // O Nome da rota -> v1/nome_controller/listarUsuario
+        [HttpGet("listarUsuario")]
         // ActionResult -> mostra o resultado de uma ação
         public IActionResult Usuarios()
         {
-            List<UsuarioModel> listaDeUsuarios = new List<UsuarioModel>();
-
             // Criando um usuario
-            UsuarioModel usuario    = new UsuarioModel();
-            usuario.id_usuario      = 1;
-            usuario.nome_usuario    = "Gabriel Moraes";
-            usuario.email           = "gmmartins06@gmail.com";
-            usuario.senha           = "123";
+            UsuarioModel usuario = new UsuarioModel();
+            usuario.id_usuario = 1;
+            usuario.nome_usuario = "Gabriel Moraes";
+            usuario.email = "gmmartins06@gmail.com";
+            usuario.senha = "123";
 
-            UsuarioModel usuario2   = new UsuarioModel();
-            usuario2.id_usuario     = 2;
-            usuario2.nome_usuario   = "Gabriella Evangelista";
-            usuario2.email          = "gabi.evangelista07@gmail.com";
-            usuario2.senha          = "456";
+            UsuarioModel usuario2 = new UsuarioModel();
+            usuario2.id_usuario = 2;
+            usuario2.nome_usuario = "Gabriella Evangelista";
+            usuario2.email = "gabi.evangelista07@gmail.com";
+            usuario2.senha = "456";
 
             // Adicionando na lista de usuarios 
 
@@ -34,6 +38,16 @@ namespace aula1.Controllers
 
             // Retornar a lista de usuarios
             return Ok(listaDeUsuarios);
+        }
+
+        [HttpGet("listarUsuario/{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            // Função que lista os usuarios 
+            Usuarios();
+
+            // Retorno dos dados que é passado por meio da URL 
+            return Ok(listaDeUsuarios.FirstOrDefault(rs => rs.id_usuario == id)); 
         }
     }
 }
