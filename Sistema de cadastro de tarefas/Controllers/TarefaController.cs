9238fe2bcf34949309c5063d42695db8afa75bb9
Gabriel Moraes <gmmartins06@gmail.com>
2019-10-21 14:27:15 -0300

    Comentarios

--- Sistema de cadastro de tarefas/Controllers/TarefaController.cs
+++ Sistema de cadastro de tarefas/Controllers/TarefaController.cs
@@ -14,6 +14,12 @@ namespace Sistema_de_cadastro_de_tarefas.Controllers
 
         TarefaContext conexao = new TarefaContext();
 
+        /// <summary>
+        ///     Lista de tarefas
+        /// </summary>
+        /// <returns>
+        ///     Função que retorna o conteudo do banco dem forma de lista
+        /// </returns>
         [HttpGet]
         public IActionResult listar()
         {
@@ -21,14 +27,32 @@ namespace Sistema_de_cadastro_de_tarefas.Controllers
             return Ok(Lista);
         }
 
+        /// <summary>
+        ///     Cadastrar no banco
+        /// </summary>
+        /// <param name="insert">
+        ///     Parametro para pegar as informações inscritas e inserir no banco
+        /// </param>
+        /// <returns>
+        ///     Retorna sucesso quando for cadastrado 
+        /// </returns>
         [HttpPost]
         public IActionResult cadastrar(TarefaModel insert)
         {
             conexao.registros_tarefas.Add(insert);
             conexao.SaveChanges();
-            return Ok();
+            return Ok("Cadastro realizado com sucesso");
         }
-
+        
+        /// <summary>
+        ///     Atualizar dados no banco
+        /// </summary>
+        /// <param name="update">
+        ///     Parametro para atualizar os registros
+        /// </param>
+        /// <returns>
+        ///     Retorna sucesso quando for atualizado
+        /// </returns>
         [HttpPut]
         public IActionResult Atualizar(TarefaModel update)
         {
@@ -45,9 +69,18 @@ namespace Sistema_de_cadastro_de_tarefas.Controllers
 
             conexao.registros_tarefas.Update(up);
             conexao.SaveChanges();
-            return Ok();
+            return Ok("Atualização realizada com sucesso");
         }
 
+        /// <summary>
+        ///     Deletar usuario
+        /// </summary>
+        /// <param name="id">
+        ///     Trazer o id do usuario por meio da url para deletar
+        /// </param>
+        /// <returns>
+        ///     Sucesso quando for excluido
+        /// </returns>
         [HttpDelete("{id}")]
         public IActionResult delete(int id)
         {
@@ -59,7 +92,7 @@ namespace Sistema_de_cadastro_de_tarefas.Controllers
 
             conexao.registros_tarefas.Remove(del);
             conexao.SaveChanges();
-            return Ok();
+            return Ok("Excluido com sucesso");
         }
 
     }
